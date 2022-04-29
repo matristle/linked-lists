852fb59a37f13f3c7dde53a4ae0b552ccb19b1e4
matristle <mercerary@tutanota.com>
2022-04-29 23:39:09 +0300

    Clean up methods with linter

--- linked_list.rb
+++ linked_list.rb
@@ -1,117 +1,107 @@
-require_relative "node.rb"
+require_relative 'node'
 
 class LinkedList
+  def initialize(head = nil)
+    @head = head
+  end
 
-    def initialize(head = nil)
-        @head = head
+  def append(value)
+    temp_node = @head
+
+    temp_node = temp_node.next_node until temp_node.next_node.nil?
+
+    temp_node.next_node = Node.new(value, nil)
+  end
+
+  def prepend(value)
+    temp_node = @head
+
+    @head = Node.new(value, temp_node)
+  end
+
+  def size
+    temp_node = @head
+    count = 0
+
+    until temp_node.nil?
+      count += 1
+      temp_node = temp_node.next_node
+    end
+    count
+  end
+
+  def head
+    @head
+  end
+
+  def tail
+    temp_node = @head
+
+    temp_node = temp_node.next_node until temp_node.next_node.nil?
+
+    temp_node
+  end
+
+  def at(index)
+    temp_node = @head
+
+    index.times do
+      temp_node = temp_node.next_node
     end
 
-    def append(value)
-        temp_node = @head
+    temp_node
+  end
 
-        until temp_node.next_node.nil? do
-            temp_node = temp_node.next_node
-        end 
+  def pop
+    temp_node = @head
+    until temp_node.next_node.next_node.nil?
+      temp_node = temp_node.next_node
+      if temp_node.next_node.next_node.nil?
+        temp_node.next_node = nil
+        break
+      end
+    end
+  end
 
-        temp_node.next_node = Node.new(value, nil)
+  def contains?(value)
+    temp_node = @head
+
+    size.times do
+      return true if temp_node.value == value
+
+      temp_node = temp_node.next_node
     end
 
-    def prepend(value)
-        temp_node = @head
+    false
+  end
 
-        @head = Node.new(value, temp_node)
+  def find(value)
+    temp_node = @head
+    size.times do |index|
+      return index if temp_node.value == value
+
+      temp_node = temp_node.next_node
     end
 
-    def size
-        temp_node = @head
-        count = 0
+    nil
+  end
 
-        until temp_node.nil? do
-            count += 1
-            temp_node = temp_node.next_node
-        end
-        
-        count
+  def to_s
+    # ( value ) -> ( value ) -> ( value ) -> nil
+    string = String.new
+
+    size.times do |index|
+      string << if index == size - 1
+                  'nil'
+                else
+                  "( #{at(index).value} ) -> "
+                end
     end
-
-    def head
-        @head
-    end
-
-    def tail
-        temp_node = @head
-        
-        until temp_node.next_node.nil? do
-            temp_node = temp_node.next_node
-        end
-
-        temp_node
-    end
-
-    def at(index)
-        temp_node = @head
-
-        index.times do 
-            temp_node = temp_node.next_node
-        end
-
-        temp_node
-    end
-
-    def pop
-        temp_node = @head
-        until temp_node.next_node.next_node.nil?
-            temp_node = temp_node.next_node
-            if temp_node.next_node.next_node.nil?
-                temp_node.next_node = nil
-                break
-            end
-        end
-
-        # head.next_node.next_node.next_node = nil
-
-    end 
-
-    def contains?(value)
-        temp_node = @head
-
-        self.size.times do
-            if temp_node.value == value
-                return true
-                break
-            end 
-            temp_node = temp_node.next_node
-        end
-
-        return false
-    end
-
-    def find(value)
-        temp_node = @head
-        self.size.times do |index|
-            if temp_node.value == value
-                return index
-                break
-            end 
-            temp_node = temp_node.next_node
-        end
-
-        return nil
-    end
-    
-    def to_s
-        # ( value ) -> ( value ) -> ( value ) -> nil
-        string = String.new
-
-        self.size.times do |index|
-            if index == self.size - 1
-                string << "nil"
-            else
-                string << "( #{self.at(index).value} ) -> "
-            end
-        end
-    end
+    string
+  end
 end
+
+#---------------------------------------------------------
 
 list = LinkedList.new
 
@@ -120,21 +110,21 @@ list.prepend('Sunny')
 list.prepend('Theo')
 list.append('Angelica')
 
-# p list
-# p list.size
-# p list.tail
+p list
+p list.size
+p list.tail
 
-# list.size.times do |index| 
-#     p list.at(index)
-# end
-# p list
+list.size.times do |index|
+  p list.at(index)
+end
+p list
 
-# p list.contains?('Theo')
-# p list.contains?('Chloe')
+p list.contains?('Theo')
+p list.contains?('Chloe')
 
-# p list.find('Angelica')
+p list.find('Angelica')
 
-# p list.to_s
+p list.to_s
 
 list.pop
 p list

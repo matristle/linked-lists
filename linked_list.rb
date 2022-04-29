2b2705ba224e166364c1c1fd3a093545a9255b54
matristle <mercerary@tutanota.com>
2022-04-29 17:00:35 +0300

    Add basic tests to methods made so far

--- linked_list.rb
+++ linked_list.rb
@@ -50,3 +50,16 @@ class LinkedList
 
     
 end
+
+list = LinkedList.new
+
+list.prepend('Sally')
+list.prepend('Sunny')
+list.prepend('Theo')
+
+list.append('Angelica')
+
+p list.size
+p list
+
+p list.tail
